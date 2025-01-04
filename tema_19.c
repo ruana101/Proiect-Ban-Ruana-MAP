1ce88e8a6c641fb9ee4dfa7cc906e32756e8abac
ruana101 <ruanadalia@gmail.com>
2025-01-04 23:38:59 +0200

    Am implementat functia pentru inserarea unui nod in arbore si functia traversarii in preordine.

--- tema_19.c
+++ tema_19.c
@@ -21,9 +21,45 @@ Nod* initializare(int cheie)
     return nod;
 }
 
+Nod* inserare(Nod* crt_nod, int cheie)
+{
+    if(crt_nod==NULL)
+        return initializare(cheie);
+    
+    if(cheie>=crt_nod->cheie)
+        crt_nod->dr=inserare(crt_nod->dr,cheie);
+    else 
+        crt_nod->st=inserare(crt_nod->st,cheie);
+    
+    return crt_nod;
+}
+
+void preordine(Nod* crt_nod)
+{
+    if(crt_nod==NULL)
+        return;
+
+    printf("%d",crt_nod->cheie);
+    preordine(crt_nod->st);
+    preordine(crt_nod->dr);
+}
+
+
 int main()
 {
     Nod* radacina=initializare(5);
+    inserare(radacina,3);
+    inserare(radacina,8);
+    inserare(radacina,2);
+    inserare(radacina,4);
+    inserare(radacina,1);
+    inserare(radacina,7);
+    inserare(radacina,9);
+    inserare(radacina,6);
+
+    printf("Traversare in pre-ordine: ");
+    preordine(radacina);
+    printf("\n");
     
     return 0;
 }
